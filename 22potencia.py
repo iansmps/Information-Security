1449677eef437352e18fcbcc48beafe03bde199a
Ian Resende <iansmps@gmail.com>
2018-12-07 16:22:39 -0200

    a

--- 22potencia.py
+++ 22potencia.py
@@ -5,17 +5,32 @@ def pot(p):
     b = 2
     listpot = [2]
     a = 2
-    while b < p:
+    start_time = time.time()
+    while b <= p/2:
         b = b*2
         a = a * a
         listpot.append(a)
-    print(listpot)
-    #print(listpot[24]**4) 
-    b = b/2
+    #print(listpot)
+    print("1--- %s seconds ---" % (time.time() - start_time))
+    start_time = time.time()
+    primo = p
+    res = 1
+    #p = p-b
     while p != 0:
-        p = p-b
-        pot = listpot[b]
+        b = b//2
+        #tam = len(listpot)
+        fat = listpot.pop()
+        #print(fat)
+
+        while p >= b:
+            res = res*fat
+            p = p - b
+    print("2--- %s seconds ---" % (time.time() - start_time))
+    res = (res % primo) - 2
+    return res
+            
+
         
+a = int(input())
 
-
-pot(10)
+print (pot(a))
